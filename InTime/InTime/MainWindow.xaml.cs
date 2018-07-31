2caaa5b22ed49095dd533c8b6d1d766adadfa7e7
Administrator <Administrator@DESKTOP-NFE9Q08>
2018-07-31 17:16:27 +0200

    aggiunta progetto

---
+++ InTime/InTime/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InTime
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        inTimeDbEntities intimeDb = new inTimeDbEntities();
+
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+            GetDbData();
+        }
+
+        // aggiungere un nuovo progetto al database
+        private void NewProjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            string projectName = NewProjectNameBox.Text;
+
+            if (projectName != "")
+            {
+                InTime.Project newProject = new InTime.Project();
+                newProject.ProjectName = projectName;
+
+                intimeDb.Projects.Add(newProject);
+                intimeDb.SaveChanges();
+
+                GetDbData();
+                NewProjectNameBox.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Il nome del progetto non è stato inserito. Inserire un nome progetto valido, quindi riprovare.","Nome progetto non valido",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow settingsWindow = new SettingsWindow();
+            settingsWindow.Show();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GetDbData()
+        {
+            DbSet<Project> projectList = intimeDb.Projects;
+
+            var query =
+                from Project in projectList
+                orderby Project.Id
+                select Project.ProjectName;
+
+            List<string> list = query.ToList();
+
+            ProjectList.Items.Clear();
+
+            foreach(string projectName in list)
+            {
+                ListBoxItem itm = new ListBoxItem();
+                itm.Content = projectName;
+                ProjectList.Items.Add(itm);
+                itm.Selected += new RoutedEventHandler(ListBoxItem_Selected);
+            }
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem itm = (ListBoxItem)sender;
+
+            MessageBox.Show(itm.Content.ToString());
+        }
+    }
+}
---
+++ InTime/SQLQueryCreazioneDatabase.sql
@@ -0,0 +1,26 @@
+﻿DROP TABLE TimeTrack;
+DROP TABLE Person;
+DROP TABLE Project;
+
+CREATE TABLE [dbo].[Person] (
+    [Id]         INT          NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [PersonName] VARCHAR (30) NOT NULL,
+	[AccessCode] UNIQUEIDENTIFIER DEFAULT(NEWID())
+);
+
+CREATE TABLE [dbo].[Project] (
+    [Id]          INT           NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [ProjectName] VARCHAR (30)  NOT NULL,
+    [ProjectAssignedTime]   TIME (7),
+    [Description] VARCHAR (MAX) NULL,
+);
+
+CREATE TABLE [dbo].[TimeTrack] (
+    [Id]          INT      NOT NULL PRIMARY KEY IDENTITY(1,1),
+    [ProjectId]   INT      NOT NULL,
+    [PersonId]    INT      NOT NULL,
+	[AssignedTime] INT,
+    [WorkTime] TIME (7) NOT NULL,
+    CONSTRAINT [FK_Time_ToPerson] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person] ([Id]),
+    CONSTRAINT [FK_Time_ToProject] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Project] ([Id])
+);
