2691b1f6042539265863a10deddbb98345c2b2e7
Alessandro Gambacorta <ale.gambacorta@hotmail.it>
2018-08-27 18:31:32 +0200

    TimeProject

--- InTime/InTime/MainWindow.xaml.cs
+++ InTime/InTime/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using Hardcodet.Wpf.TaskbarNotification;
+using System.Diagnostics;
 
 namespace InTime
 {
@@ -20,29 +22,47 @@ namespace InTime
     /// </summary>
     public partial class MainWindow : Window
     {
-        private NotifyIcon notifyIcon;
+
+        NotifyIcon inTimeIcon = new NotifyIcon();
+
+        Stopwatch timeProject = new Stopwatch();
 
         public MainWindow()
         {
-            notifyIcon = new NotifyIcon();
-            notifyIcon.Visible = true;
-            notifyIcon.DoubleClick += new System.EventHandler(this.notifyIcon_DoubleClick);
+            InitializeComponent();
+
+            this.Hide();
+            inTimeIcon.DoubleClick += new EventHandler(this.inTimeIcon_DoubleClick);
+
+            inTimeIcon.Visible = true;
+
+            inTimeIcon.Icon = InTime.Properties.Resources.InTimeIcon;            
         }
 
-        private void notifyIcon_DoubleClick(object sender, EventArgs e)
+        private void inTimeIcon_DoubleClick(object sender, EventArgs e)
         {
-            if (this.WindowState == WindowState.Minimized)
-            {
-                WindowState = WindowState.Normal;
-                InitializeComponent();
-            }
-            this.Activate();
+                this.Show();
+                this.WindowState = WindowState.Normal;
         }
 
         private void AdministratorButton_Click(object sender, RoutedEventArgs e)
         {
             AdministratorWindow administratorWindow = new AdministratorWindow();
             administratorWindow.Show();
+        }
+
+        private void TimeProject1_Click(object sender, RoutedEventArgs e)
+        {
+            TimeSpan ts = timeProject.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            TimeTextBlock.Text = elapsedTime;
+        }
+
+        private void TimeProject2_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
