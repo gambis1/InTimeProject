6f4b8cfc279d9d3fdc65f22ccc22107473f54063
Tom G <tommag21@gmail.com>
2018-09-01 13:36:16 +0200

    progressi timetracker, suddivisione mainwindow.cs

---
+++ InTime/InTime/Logic/TimeTracker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InTime.Logic
+{
+    class TimeTracker
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private DateTime trackingDate;
+        private TimeSpan startingTime;
+        private int projectId;
+
+        public TimeTracker(int projectId)
+        {
+            this.projectId = projectId;
+        }
+
+        public void Start()
+        {
+            // TO DO: query sul nome del progetto: 'latestDate' = ultima data registrata
+
+            trackingDate = new DateTime(2018, 9, 1); // DEBUG,HARDCODED
+
+            if (trackingDate.Date != DateTime.Now.Date) // data non presente in database: nuova data, startingtime = 0
+            {
+                DateTime newDate = DateTime.Now.Date;
+                startingTime = new TimeSpan(0, 0, 0);
+            } else // data già presente: query sul tempo già tracciato
+            {
+                // TO DO: query per tempo tracciato
+
+                startingTime = new TimeSpan(1, 30, 0); // DEBUG,HARDCODED
+            }
+
+            stopwatch.Reset();
+            stopwatch.Start();
+        }
+
+        public TimeSpan Update()
+        {
+            if (this.IsRunning)
+            {
+                // registrare sul database
+
+                TimeSpan trackedTime = stopwatch.Elapsed + startingTime;
+                return trackedTime;
+            } else
+            {
+                return startingTime;
+            }
+        }
+
+        public void Stop()
+        {
+            if (this.IsRunning) {
+                Update();
+                stopwatch.Stop();
+            }
+        }
+
+        public bool IsRunning
+        {
+            get { return stopwatch.IsRunning; }
+        }
+    }
+}
--- InTime/InTime/MainWindow.xaml.cs
+++ InTime/InTime/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.Diagnostics;
 using System.Threading;
 using InTime.User;
 using InTime.Admin;
+using InTime.Logic;
+using System.Windows.Threading;
 
 namespace InTime
 {
@@ -26,28 +28,26 @@ namespace InTime
     {
         NotifyIcon inTimeIcon = new NotifyIcon();
         Stopwatch timeProject = new Stopwatch();
+        TimeTracker timeTracker;
 
         public MainWindow()
         {
             InitializeComponent();
             this.Loaded += new RoutedEventHandler(WindowPositioned);
-
-            // ICONA
             this.Hide();
-            inTimeIcon.Icon = new System.Drawing.Icon("../../Resources/StoppedIcon.ico");
-            // vecchio percorso:
-            // InTime.Properties.Resources.InTimeIcon;
+
+            inTimeIcon.Icon = new System.Drawing.Icon("../../Resources/StoppedIcon.ico"); // vecchio percorso icona: InTime.Properties.Resources.InTimeIcon;
             inTimeIcon.Visible = true;
-
             inTimeIcon.Text = "Timer fermo";
-
             inTimeIcon.Click += new EventHandler(this.inTimeIcon_Click);            
         }
 
-        // POSIZIONE FINESTRA
-        private void WindowPositioned(object sender, RoutedEventArgs e)
+
+        /*-------------------------------------------------------------- FINESTRA --------------------------------------------------------------*/
+
+        private void WindowPositioned(object sender, RoutedEventArgs e) // posizione finestra sopra icona
         {
-            // RILEVAZIONE POSIZIONE MOUSE
+            // rilevazione posizione mouse
             System.Drawing.Point point = System.Windows.Forms.Control.MousePosition;
             var mousePosition = new System.Windows.Point(point.X, point.Y);
             var transform = PresentationSource.FromVisual(this).CompositionTarget.TransformFromDevice;
@@ -58,25 +58,32 @@ namespace InTime
             this.Top = desktopWorkingArea.Bottom - this.Height;
         }
 
-        // CLICK SU ICONA
-        private void inTimeIcon_Click(object sender, EventArgs e)
+        private void inTimeIcon_Click(object sender, EventArgs e) // apertura finestra al click su icona
         {
             this.Show();
-            this.Activate(); // focus su finestra
+            this.Activate(); // focus sulla finestra
             this.WindowState = WindowState.Normal;
         }
 
-        // FOCUS FUORI DALLA FINESTRA PER NASCONDERLA
-        protected override void OnDeactivated(EventArgs e)
+        protected override void OnDeactivated(EventArgs e) // nascondere finestra quando perde focus (es. click fuori)
         {
             base.OnDeactivated(e);
             this.Hide();
         }
 
+
+        /*-------------------------------------------------------------- TASTI MENU --------------------------------------------------------------*/
+
         private void AdministratorButton_Click(object sender, RoutedEventArgs e)
         {
             AdministratorWindow administratorWindow = new AdministratorWindow();
             administratorWindow.Show();
+        }
+
+        private void UserButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserWindow userWindow = new UserWindow();
+            userWindow.Show();
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
@@ -92,6 +99,9 @@ namespace InTime
             }            
         }
 
+
+        /*-------------------------------------------------------------- TASTO PROGETTO 1 --------------------------------------------------------------*/
+
         private void TimeProject1_Click(object sender, RoutedEventArgs e)
         {
             Thread thread = new Thread(new ThreadStart(RunTime));
@@ -101,31 +111,46 @@ namespace InTime
             inTimeIcon.Text = "Progetto 1: 00:00";
         }
 
-        private void TimeProject2_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void RunTime()
         {
-            TimeSpan ts = timeProject.Elapsed;
-            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
-            timeProject.Start();
+            timeTracker = new TimeTracker(1);
+            timeTracker.Start();
+            //TimeSpan ts = timeProject.Elapsed;
+            //string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+            //timeProject.Start();
             //TimeTextBlock.Text = elapsedTime;
         }
+
+
+        /*-------------------------------------------------------------- TASTO PROGETTO 2 --------------------------------------------------------------*/
+
+        // test dispatcher
+
+        private void TimeProject2_Click(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Project2Time.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        /*-------------------------------------------------------------- TASTO STOP --------------------------------------------------------------*/
 
         private void StopTimer_Click(object sender, RoutedEventArgs e)
         {
             // TO DO: metodo per fermare il timer
+            //Console.WriteLine(timeTracker.Update().ToString());
+            //timeTracker.Stop();
 
             inTimeIcon.Icon = new System.Drawing.Icon("../../Resources/StoppedIcon.ico");
             inTimeIcon.Text = "Timer fermo";
         }
 
-        private void UserButton_Click(object sender, RoutedEventArgs e)
-        {
-            UserWindow userWindow = new UserWindow();
-            userWindow.Show();
-        }
+
     }
 }
